8f5d9b276e4db362c0f96a6a1ebfbd2137b1553d
Vedika365 <vedikajain873@yahoo.com>
2024-12-08 23:02:28 -0500

    third commit

---
+++ src/average/Average_Driver.java
@@ -0,0 +1,34 @@
+package average;
+
+public class Average_Driver {
+
+
+        public static void main(String [] args){
+            Average average = new Average();
+
+            System.out.println(average.toString());
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
